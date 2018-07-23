615ed9512b312d4a6e6d5cabb1b89607202353fe
Roja1912 <roja.t@accionlabs.com>
2018-07-23 18:16:09 +0530

    msg

---
+++ assignment/day.js
@@ -0,0 +1,13 @@
+window.onload=function(){
+    var button=document.getElementById("btn");
+    button.onclick=function(){
+      var day= ["sunday","monday","tuesday","wednesday","thursday","friday","saturday"];
+      
+      var date= new Date(document.getElementById("date").value);
+     
+      var n= date.getDay();
+     
+      document.write(day[n]);
+
+    }
+}    
